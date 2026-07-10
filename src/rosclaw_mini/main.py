322c4d2fb263616868adac47b828f0621c1745db
alex <1773269072@qq.com>
2026-07-10 15:38:35 +0800

    uuid随机id

--- src/rosclaw_mini/main.py
+++ src/rosclaw_mini/main.py
@@ -1,10 +1,11 @@
 from rosclaw_mini.skills.registry import BUILTIN_SKILLS
 from rosclaw_mini.gateway.command.gateway import run_command
 from rosclaw_mini.llm.command_parser import parse_command
+import uuid
 
 def main():
     command=input("请输入指令：")
-    cmd_id="cmd-001"
+    cmd_id=str(uuid.uuid4())
     cmd_1 = parse_command(command, cmd_id)
     execute=run_command(cmd_1, BUILTIN_SKILLS)
     print(execute)
